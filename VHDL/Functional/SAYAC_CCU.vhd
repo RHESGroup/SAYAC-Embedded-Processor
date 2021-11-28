c9887bededd12a0ad5af940e43b980b05a57879c
HaniehTAsl <69580750+HaniehTAsl@users.noreply.github.com>
2021-11-28 11:45:43 +0330

    Update SAYAC_CCU.vhd

--- VHDL/Functional/SAYAC_CCU.vhd
+++ VHDL/Functional/SAYAC_CCU.vhd
@@ -1,10 +1,10 @@
 --******************************************************************************
---	Filename:		SAYAC_CCU.vhd
---	Project:		SAYAC : Simple Architecture Yet Ample Circuitry
+--  Filename:		SAYAC_CCU.vhd
+--  Project:		SAYAC : Simple Architecture Yet Ample Circuitry
 --  Version:		0.990
---	History:
---	Date:			18 June 2021
---	Last Author: 	HANIEH
+--  History:
+--  Date:		18 June 2021
+--  Last Author: 	HANIEH
 --  Copyright (C) 2021 University of Tehran
 --  This source file may be used and distributed without
 --  restriction provided that this copyright statement is not
@@ -27,19 +27,19 @@ ENTITY CCU IS
 		clk, rst, readyMEM, readyMDU : IN STD_LOGIC;
 		opcode   : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
 		FIB      : IN STD_LOGIC_VECTOR(4 DOWNTO 0);  --FLags Intrepretation Bits
-	    seldataBus_TRF, selPC1_TRF, selLLU_TRF, selSHU_TRF, 
+	        seldataBus_TRF, selPC1_TRF, selLLU_TRF, selSHU_TRF, 
 		selASU_TRF, selMDU1_TRF, selMDU2_TRF, selIMM_TRF,
-        selrs1_TRF, selrd_1_TRF, selrs2_TRF, selrd_2_TRF, selrd0_TRF, 
+                selrs1_TRF, selrd_1_TRF, selrs2_TRF, selrd_2_TRF, selrd0_TRF, 
 		selrd1_TRF, writeTRF, selp1_PCP, selimm_PCP, 
-        selp1_PC, selPCadd_PC, selPC1_PC,
-        selPC_MEM, selADR_MEM, driveDataBus,
-        SE5bits, SE6bits, USE8bits, SE8bits, p1lowbits,
-        selp2_ASU, selimm_ASU, arithADD, arithSUB,
-        logicAND, onesComp, twosComp,
-        selp2_SHU, selshim_SHU, logicSH, arithSH,
-        ldMDU1, ldMDU2, arithMUL, arithDIV, startMDU,
-        ldIR, ldADR, ldPC,
-        readMEM, writeMEM, readIO, writeIO,
+		selp1_PC, selPCadd_PC, selPC1_PC,
+		selPC_MEM, selADR_MEM, driveDataBus,
+		SE5bits, SE6bits, USE8bits, SE8bits, p1lowbits,
+		selp2_ASU, selimm_ASU, arithADD, arithSUB,
+		logicAND, onesComp, twosComp,
+		selp2_SHU, selshim_SHU, logicSH, arithSH,
+		ldMDU1, ldMDU2, arithMUL, arithDIV, startMDU,
+		ldIR, ldADR, ldPC,
+		readMEM, writeMEM, readIO, writeIO,
 		setFlags, enFlag : OUT STD_LOGIC;
 		outFlag  : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
 		selFlag  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
@@ -87,24 +87,24 @@ BEGIN
 	-- Issuing control signals
 	PROCESS (pstate, readyMEM, readyMDU, opcode, FIB, outFlag)
 	BEGIN
-		seldataBus_TRF <= '0';	selPC1_TRF <= '0';		selLLU_TRF <= '0'; 
-		selSHU_TRF <= '0';	    selASU_TRF <= '0';		selMDU1_TRF <= '0'; 
-		selMDU2_TRF <= '0';	    selIMM_TRF <= '0';		selrs1_TRF <= '0';	    
-		selrd_1_TRF <= '0';		selrd0_TRF <= '0';		selrd1_TRF <= '0';	    
-		writeTRF <= '0';		selp1_PCP <= '0';		selimm_PCP <= '0';	
-		selp1_PC <= '0';		selPCadd_PC <= '0';		selPC1_PC <= '0';       
-		selPC_MEM <= '0';		selADR_MEM <= '0';	    SE5bits <= '0';			
-		SE6bits <= '0';			USE8bits <= '0';		SE8bits <= '0';			
-		p1lowbits <= '0';		selp2_ASU <= '0';		selimm_ASU <= '0';		
-		arithADD <= '0';		arithSUB <= '0';        logicAND <= '0';		
-		onesComp <= '0';		twosComp <= '0';        selp2_SHU <= '0';		
-		selshim_SHU <= '0';		logicSH <= '0';			arithSH <= '0';
-		ldMDU1 <= '0';			ldMDU2 <= '0';			arithMUL <= '0';
-		arithDIV <= '0';		startMDU <= '0';        ldIR <= '0';
-		ldADR <= '0';			ldPC <= '0';	        readMEM <= '0';
-		writeMEM <= '0';		readIO <= '0';			writeIO <= '0';		
-		selrd_2_TRF <= '0';		selrs2_TRF <= '0';		driveDataBus <= '0';
-		setFlags <= '0';		enFlag <= '0';
+		seldataBus_TRF <= '0';	selPC1_TRF <= '0';	selLLU_TRF <= '0'; 
+		selSHU_TRF <= '0';	selASU_TRF <= '0';	selMDU1_TRF <= '0'; 
+		selMDU2_TRF <= '0';	selIMM_TRF <= '0';	selrs1_TRF <= '0';	    
+		selrd_1_TRF <= '0';	selrd0_TRF <= '0';	selrd1_TRF <= '0';	    
+		writeTRF <= '0';	selp1_PCP <= '0';	selimm_PCP <= '0';	
+		selp1_PC <= '0';	selPCadd_PC <= '0';	selPC1_PC <= '0';       
+		selPC_MEM <= '0';	selADR_MEM <= '0';	SE5bits <= '0';			
+		SE6bits <= '0';		USE8bits <= '0';	SE8bits <= '0';			
+		p1lowbits <= '0';	selp2_ASU <= '0';	selimm_ASU <= '0';		
+		arithADD <= '0';	arithSUB <= '0';        logicAND <= '0';		
+		onesComp <= '0';	twosComp <= '0';        selp2_SHU <= '0';		
+		selshim_SHU <= '0';	logicSH <= '0';		arithSH <= '0';
+		ldMDU1 <= '0';		ldMDU2 <= '0';		arithMUL <= '0';
+		arithDIV <= '0';	startMDU <= '0';        ldIR <= '0';
+		ldADR <= '0';		ldPC <= '0';	        readMEM <= '0';
+		writeMEM <= '0';	readIO <= '0';		writeIO <= '0';		
+		selrd_2_TRF <= '0';	selrs2_TRF <= '0';	driveDataBus <= '0';
+		setFlags <= '0';	enFlag <= '0';
 		selFlag <= (OTHERS => '0');			
 		
 		CASE (pstate) IS
@@ -126,7 +126,7 @@ BEGIN
 								selrs1_TRF <= '1';		selp1_PCP <= '1';	selPCadd_PC <= '1';	
 								ldPC <= '1';			
 								IF opcode(1) = '1' THEN
-									selrd0_TRF <= '1';		selPC1_TRF <= '1';		
+									selrd0_TRF <= '1';	selPC1_TRF <= '1';		
 									writeTRF <= '1';
 								END IF;
 							WHEN JMI =>
@@ -140,182 +140,182 @@ BEGIN
 								writeTRF <= '0';		selrd_1_TRF <= '0';		
 						END CASE;
 					WHEN ANR =>
-						selrs1_TRF <= '1';		selrs2_TRF <= '1';		selp2_ASU <= '1';	
-						logicAND <= '1';		selrd0_TRF <= '1';		selLLU_TRF <= '1';	
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';	
+						selrs1_TRF <= '1';		selrs2_TRF <= '1';	selp2_ASU <= '1';	
+						logicAND <= '1';		selrd0_TRF <= '1';	selLLU_TRF <= '1';	
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';	
 					WHEN ANI =>
-						selrd_1_TRF <= '1';		USE8bits <= '1';		selimm_ASU <= '1';	
-						logicAND <= '1';		selrd0_TRF <= '1';		selLLU_TRF <= '1';	
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';
+						selrd_1_TRF <= '1';		USE8bits <= '1';	selimm_ASU <= '1';	
+						logicAND <= '1';		selrd0_TRF <= '1';	selLLU_TRF <= '1';	
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';
 					WHEN MSI =>
-						SE8bits <= '1';			selrd0_TRF <= '1';		selIMM_TRF <= '1';
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';
+						SE8bits <= '1';			selrd0_TRF <= '1';	selIMM_TRF <= '1';
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';
 					WHEN MHI =>
-						selrd_1_TRF <= '1';		p1lowbits <= '1';		selrd0_TRF <= '1';
-						selIMM_TRF <= '1';		writeTRF <= '1';		selPC1_PC <= '1';
+						selrd_1_TRF <= '1';		p1lowbits <= '1';	selrd0_TRF <= '1';
+						selIMM_TRF <= '1';		writeTRF <= '1';	selPC1_PC <= '1';
 						ldPC <= '1';			
 					WHEN SLR =>
-						selrs1_TRF <= '1';		selrs2_TRF <= '1';		selp2_SHU <= '1';
-						logicSH <= '1';			selrd0_TRF <= '1';		selSHU_TRF <= '1';	
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';	
+						selrs1_TRF <= '1';		selrs2_TRF <= '1';	selp2_SHU <= '1';
+						logicSH <= '1';			selrd0_TRF <= '1';	selSHU_TRF <= '1';	
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';	
 					WHEN SAR =>
-						selrs1_TRF <= '1';		selrs2_TRF <= '1';		selp2_SHU <= '1';
-						arithSH <= '1';			selrd0_TRF <= '1';		selSHU_TRF <= '1';	
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';	
+						selrs1_TRF <= '1';		selrs2_TRF <= '1';	selp2_SHU <= '1';
+						arithSH <= '1';			selrd0_TRF <= '1';	selSHU_TRF <= '1';	
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';	
 					WHEN ADR =>
-						selrs1_TRF <= '1';		selrs2_TRF <= '1';		selp2_ASU <= '1';
-						arithADD <= '1';		selrd0_TRF <= '1';		selASU_TRF <= '1';	
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';	
+						selrs1_TRF <= '1';		selrs2_TRF <= '1';	selp2_ASU <= '1';
+						arithADD <= '1';		selrd0_TRF <= '1';	selASU_TRF <= '1';	
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';	
 					WHEN SUR =>
-						selrs1_TRF <= '1';		selrs2_TRF <= '1';		selp2_ASU <= '1';
-						arithSUB <= '1';		selrd0_TRF <= '1';		selASU_TRF <= '1';	
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';
+						selrs1_TRF <= '1';		selrs2_TRF <= '1';	selp2_ASU <= '1';
+						arithSUB <= '1';		selrd0_TRF <= '1';	selASU_TRF <= '1';	
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';
 					WHEN ADI =>
-						selrd_1_TRF <= '1';		SE8bits <= '1';			selimm_ASU <= '1';
-						arithADD <= '1';		selrd0_TRF <= '1';		selASU_TRF <= '1';	
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';
+						selrd_1_TRF <= '1';		SE8bits <= '1';		selimm_ASU <= '1';
+						arithADD <= '1';		selrd0_TRF <= '1';	selASU_TRF <= '1';	
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';
 					WHEN SUI =>
-						selrd_1_TRF <= '1';		SE8bits <= '1';			selimm_ASU <= '1';
-						arithSUB <= '1';		selrd0_TRF <= '1';		selASU_TRF <= '1';	
-						writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';
+						selrd_1_TRF <= '1';		SE8bits <= '1';		selimm_ASU <= '1';
+						arithSUB <= '1';		selrd0_TRF <= '1';	selASU_TRF <= '1';	
+						writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';
 					WHEN MUL =>
-						selrs1_TRF <= '1';		selrs2_TRF <= '1';		arithMUL <= '1';
+						selrs1_TRF <= '1';		selrs2_TRF <= '1';	arithMUL <= '1';
 						IF readyMDU = '1' THEN 
 							startMDU <= '1';	ldMDU1 <= '1';
 						END IF;
 					WHEN DIV =>
-						selrs1_TRF <= '1';		selrs2_TRF <= '1';		arithDIV <= '1';
+						selrs1_TRF <= '1';		selrs2_TRF <= '1';	arithDIV <= '1';
 						IF readyMDU = '1' THEN 
 							startMDU <= '1';	ldMDU1 <= '1';
 						END IF;
 					WHEN INST15 =>
 						CASE (opcode3downto1) IS
 							WHEN CMR =>
-								selrs1_TRF <= '1';		selrd_2_TRF <= '1';		selp2_ASU <= '1';		
-								selPC1_PC <= '1';		ldPC <= '1';			enFlag <= '1';
+								selrs1_TRF <= '1';		selrd_2_TRF <= '1';	selp2_ASU <= '1';		
+								selPC1_PC <= '1';		ldPC <= '1';		enFlag <= '1';
 								selFlag <= "00110000";
 							WHEN CMI =>
-								selrd_1_TRF <= '1';		SE5bits <= '1';			selimm_ASU <= '1';	
-								selPC1_PC <= '1';		ldPC <= '1';			enFlag <= '1';
+								selrd_1_TRF <= '1';		SE5bits <= '1';		selimm_ASU <= '1';	
+								selPC1_PC <= '1';		ldPC <= '1';		enFlag <= '1';
 								selFlag <= "00110000";
 							WHEN BRC =>
 								CASE (RFI) IS
 									WHEN "000" =>
 										ldPC <= '1';
-										IF outFlag(4) = '1' THEN						--eq
-											selrd_1_TRF <= '1';		selp1_PC <= '1';
+										IF outFlag(4) = '1' THEN			--eq
+											selrd_1_TRF <= '1';	selp1_PC <= '1';
 										ELSE
 											selPC1_PC <= '1';		
 										END IF;
 									WHEN "001" =>
 										ldPC <= '1';
 										IF outFlag(5) = '0' AND outFlag(4) = '0' THEN	--lt
-											selrd_1_TRF <= '1';		selp1_PC <= '1';	
+											selrd_1_TRF <= '1';	selp1_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';
 										END IF;
 									WHEN "010" =>
 										ldPC <= '1';
-										IF outFlag(5) = '1' THEN						--gt
-											selrd_1_TRF <= '1';		selp1_PC <= '1';	
+										IF outFlag(5) = '1' THEN			--gt
+											selrd_1_TRF <= '1';	selp1_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';
 										END IF;
 									WHEN "011" =>
 										ldPC <= '1';
 										IF outFlag(5) = '1' OR outFlag(4) = '1' THEN	--gt/eq
-											selrd_1_TRF <= '1';		selp1_PC <= '1';	
+											selrd_1_TRF <= '1';	selp1_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';
 										END IF;
 									WHEN "100" =>
 										ldPC <= '1';
 										IF outFlag(5) = '0' OR outFlag(4) = '1' THEN	--lt/eq
-											selrd_1_TRF <= '1';		selp1_PC <= '1';
+											selrd_1_TRF <= '1';	selp1_PC <= '1';
 										ELSE
 											selPC1_PC <= '1';		
 										END IF;
 									WHEN "101" =>
 										ldPC <= '1';
-										IF outFlag(4) = '0' THEN						--neq
-											selrd_1_TRF <= '1';		selp1_PC <= '1';	
+										IF outFlag(4) = '0' THEN			--neq
+											selrd_1_TRF <= '1';	selp1_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';
 										END IF;
 									WHEN OTHERS =>
-										selrd_1_TRF <= '0';		selp1_PC <= '0';	ldPC <= '0';
+										selrd_1_TRF <= '0';	selp1_PC <= '0';	ldPC <= '0';
 										selPC1_PC <= '0';
 								END CASE;
 							WHEN BRR =>
 								CASE (RFI) IS
 									WHEN "000" =>
 										ldPC <= '1';
-										IF outFlag(4) = '1' THEN						--eq
-											selrd_1_TRF <= '1';		selp1_PCP <= '1';		selPCadd_PC <= '1';	
+										IF outFlag(4) = '1' THEN			--eq
+											selrd_1_TRF <= '1';	selp1_PCP <= '1';	selPCadd_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';
 										END IF;
 									WHEN "001" =>
 										ldPC <= '1';
 										IF outFlag(5) = '0' AND outFlag(4) = '0' THEN	--lt
-											selrd_1_TRF <= '1';		selp1_PCP <= '1';		selPCadd_PC <= '1';	
+											selrd_1_TRF <= '1';	selp1_PCP <= '1';	selPCadd_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';											
 										END IF;
 									WHEN "010" =>
 										ldPC <= '1';
-										IF outFlag(5) = '1' THEN						--gt
-											selrd_1_TRF <= '1';		selp1_PCP <= '1';		selPCadd_PC <= '1';	
+										IF outFlag(5) = '1' THEN			--gt
+											selrd_1_TRF <= '1';	selp1_PCP <= '1';	selPCadd_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';		
 										END IF;
 									WHEN "011" =>
 										ldPC <= '1';
 										IF outFlag(5) = '1' OR outFlag(4) = '1' THEN	--gt/eq
-											selrd_1_TRF <= '1';		selp1_PCP <= '1';		selPCadd_PC <= '1';	
+											selrd_1_TRF <= '1';	selp1_PCP <= '1';	selPCadd_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';
 										END IF;
 									WHEN "100" =>
 										ldPC <= '1';
 										IF outFlag(5) = '0' OR outFlag(4) = '1' THEN	--lt/eq
-											selrd_1_TRF <= '1';		selp1_PCP <= '1';		selPCadd_PC <= '1';	
+											selrd_1_TRF <= '1';	selp1_PCP <= '1';	selPCadd_PC <= '1';	
 										ELSE
 											selPC1_PC <= '1';
 										END IF;
 									WHEN "101" =>
 										ldPC <= '1';
-										IF outFlag(4) = '0' THEN						--neq
-											selrd_1_TRF <= '1';		selp1_PC <= '1';		selPCadd_PC <= '1';
+										IF outFlag(4) = '0' THEN			--neq
+											selrd_1_TRF <= '1';	selp1_PC <= '1';	selPCadd_PC <= '1';
 										ELSE
 											selPC1_PC <= '1';	
 										END IF;
 									WHEN OTHERS =>
-										selrd_1_TRF <= '0';		selp1_PCP <= '0';		selPCadd_PC <= '0';	
+										selrd_1_TRF <= '0';		selp1_PCP <= '0';	selPCadd_PC <= '0';	
 										ldPC <= '0';
 								END CASE;
 							WHEN NTR =>
-								selrs1_TRF <= '1';		selrd0_TRF <= '1';		selLLU_TRF <= '1';	
-								writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';
+								selrs1_TRF <= '1';		selrd0_TRF <= '1';	selLLU_TRF <= '1';	
+								writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';
 								IF opcode(0) = '0' THEN
 									onesComp <= '1';
 								ELSE
 									twosComp <= '1';
 								END IF;
 							WHEN NTD =>
-								selrd_1_TRF <= '1';		selrd0_TRF <= '1'; 		selLLU_TRF <= '1';	
-								writeTRF <= '1';		selPC1_PC <= '1';		ldPC <= '1';
+								selrd_1_TRF <= '1';		selrd0_TRF <= '1'; 	selLLU_TRF <= '1';	
+								writeTRF <= '1';		selPC1_PC <= '1';	ldPC <= '1';
 								IF opcode(0) = '0' THEN
 									onesComp <= '1';
 								ELSE
 									twosComp <= '1';
 								END IF;
 							WHEN OTHERS =>
-								selrs1_TRF <= '0';		selp2_ASU <= '0';		SE5bits <= '0';	
-								selimm_ASU <= '0';		selrd_1_TRF <= '0';		selp1_PC <= '0';	
-								writeTRF <= '0';		selPCadd_PC <= '0';		selLLU_TRF <= '0';	
-								selrd0_TRF <= '0';		onesComp <= '0';		twosComp <= '0';	
-								ldPC <= '0';			selrs2_TRF <= '0';		enFlag <= '0';
+								selrs1_TRF <= '0';		selp2_ASU <= '0';	SE5bits <= '0';	
+								selimm_ASU <= '0';		selrd_1_TRF <= '0';	selp1_PC <= '0';	
+								writeTRF <= '0';		selPCadd_PC <= '0';	selLLU_TRF <= '0';	
+								selrd0_TRF <= '0';		onesComp <= '0';	twosComp <= '0';	
+								ldPC <= '0';			selrs2_TRF <= '0';	enFlag <= '0';
 								selFlag <= (OTHERS => '0');
 						END CASE;
 					WHEN OTHERS =>
@@ -357,14 +357,14 @@ BEGIN
 								selrd0_TRF <= '1';
 								IF opcode(1) = '1' THEN		--I/O
 									readIO <= '1';	ldPC <= '1';	writeTRF <= '1';
-								ELSE						--MEM
+								ELSE				--MEM
 									readMEM <= '1';
 									IF readyMEM = '1' THEN
 										ldPC <= '1';	writeTRF <= '1';
 									END IF;
 								END IF;
 							WHEN STR =>
-								selADR_MEM <= '1';	selrs1_TRF <= '1';		driveDataBus <= '1';
+								selADR_MEM <= '1';	selrs1_TRF <= '1';	driveDataBus <= '1';
 								selPC1_PC <= '1';
 								IF opcode(1) = '1' THEN
 									writeIO <= '1';		ldPC <= '1';
@@ -380,21 +380,21 @@ BEGIN
 								writeMEM<= '0';
 						END CASE;
 					WHEN MUL | DIV => 
-						selrd0_TRF <= '1';		selMDU1_TRF <= '1';		writeTRF <= '1';
+						selrd0_TRF <= '1';		selMDU1_TRF <= '1';	writeTRF <= '1';
 						ldMDU2 <= '1';
 					WHEN OTHERS =>
-						selADR_MEM <= '0';		selrd0_TRF <= '0';		readIO <= '0';	
-						writeTRF <= '0';		readMEM <= '0';			writeIO <= '0';	
-						writeMEM<= '0';			arithMUL <= '0';		selMDU1_TRF <= '0';	
-						ldMDU1 <= '0';			arithDIV <= '0';		driveDataBus <= '0';
+						selADR_MEM <= '0';		selrd0_TRF <= '0';	readIO <= '0';	
+						writeTRF <= '0';		readMEM <= '0';		writeIO <= '0';	
+						writeMEM<= '0';			arithMUL <= '0';	selMDU1_TRF <= '0';	
+						ldMDU1 <= '0';			arithDIV <= '0';	driveDataBus <= '0';
 				END CASE;
 			WHEN exec3 =>
 				CASE (opcode7downto4) IS
 					WHEN MUL | DIV => 
-						selrd1_TRF <= '1';		selMDU2_TRF <= '1';		writeTRF <= '1';
+						selrd1_TRF <= '1';		selMDU2_TRF <= '1';	writeTRF <= '1';
 						selPC1_PC <= '1';		ldPC <= '1';
 					WHEN OTHERS => 
-						writeTRF <= '0';		selMDU2_TRF <= '0';		selrd1_TRF <= '0';	
+						writeTRF <= '0';		selMDU2_TRF <= '0';	selrd1_TRF <= '0';	
 						selPC1_PC <= '0';		ldPC <= '0';
 				END CASE;
 		END CASE;
@@ -450,7 +450,7 @@ BEGIN
 							WHEN LDR | STR =>	
 								IF opcode(1) = '1' THEN		--I/O
 									nstate <= fetch;
-								ELSE						--MEM
+								ELSE				--MEM
 									IF readyMEM = '1' THEN
 										nstate <= fetch;
 									ELSE
